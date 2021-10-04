4b1a6727b66724adb51e7721a44398223cde6704
Alex Abrahamsson <alex.abrahamsson@gmail.com>
2021-10-04 20:02:58 +0200

    kult

--- main.js
+++ main.js
@@ -11,22 +11,16 @@ else                                                               // Finns det 
 
 let weatherData = {
   apiKey2: "941bac0f44203ce92ef6aa74c6455d8e",
-  FetchWeather: function (city) {
-    document.body.style.backgroundImage =
-      "url('https://source.unsplash.com/1600x900/?" + city + "')";    // Hämtar en bild som är baserad på Staden vi kollar väder i
+  FetchWeather: function (city) 
+  {
+    document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + city + "')";    // Hämtar en bild som är baserad på Staden vi kollar väder i
     document.getElementById("locationName").innerHTML = city;         // Visar namnet på den stad vi kollar väder i
-    fetch(
-      "http://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=metric&appid=" +
-        this.apiKey2
-    )
+    fetch("http://api.openweathermap.org/data/2.5/weather?q=" +city +"&units=metric&appid=" +this.apiKey2)
       .then((respons) => respons.json())
       .then((data2) => this.getInfo(data2));
   },
-  getInfo(
-    data2                                                                  // Hämtar lat och Long ur första Api'n för att kunna få fram stad utan att använda google Api som kanske kostar pengar
-  ) {
+  getInfo(data2)            // Hämtar lat och Long ur första Api'n för att kunna få fram stad utan att använda google Api som kanske kostar pengar
+  {
     latitude = "lat=" + data2.coord.lat + "&";
     longitude = "lon=" + data2.coord.lon + "&";
     queryUrl = "https://api.openweathermap.org/data/2.5/onecall?";
@@ -38,14 +32,10 @@ let weatherData = {
       .then((response) => response.json())
       .then((data) => {
         //Main window--
-        document.getElementById("Description").innerHTML =
-          data.current.weather[0].main;
-        document.getElementById("TempNow").innerHTML =
-          Math.round(data.current.temp) + "°C";
-        document.getElementById("Windspeed").innerHTML =
-          "Wind speed: " + data.current.wind_speed;
-        document.getElementById("Humidity").innerHTML =
-          "Humidity: " + data.current.humidity + " %";
+        document.getElementById("Description").innerHTML = data.current.weather[0].main;
+        document.getElementById("TempNow").innerHTML = Math.round(data.current.temp) + "°C";
+        document.getElementById("Windspeed").innerHTML = "Wind speed: " + data.current.wind_speed;
+        document.getElementById("Humidity").innerHTML = "Humidity: " + data.current.humidity + " %";
 
         // ================== Hämtar klockslag ====================================
         let timeNow = new Date().getHours();
@@ -88,105 +78,84 @@ let weatherData = {
         let iconBase = "http://openweathermap.org/img/wn/";
         // ========================================= Väder timme för timme ============================================
         document.getElementById("TimeHour1").innerHTML = time1;
-        document.getElementById("Hourly-icon1").src =
-          iconBase + data.hourly[1].weather[0].icon + ".png";
-        document.getElementById("TempHour1").innerHTML =
-          data.hourly[1].temp + "°";
+        document.getElementById("Hourly-icon1").src = iconBase + data.hourly[1].weather[0].icon + ".png";
+        document.getElementById("TempHour1").innerHTML = data.hourly[1].temp + "°";
 
         document.getElementById("TimeHour2").innerHTML = time2;
-        document.getElementById("Hourly-icon2").src =
-          iconBase + data.hourly[2].weather[0].icon + ".png";
-        document.getElementById("TempHour2").innerHTML =
-          data.hourly[2].temp + "°";
+        document.getElementById("Hourly-icon2").src = iconBase + data.hourly[2].weather[0].icon + ".png";
+        document.getElementById("TempHour2").innerHTML = data.hourly[2].temp + "°";
 
         document.getElementById("TimeHour3").innerHTML = time3;
-        document.getElementById("Hourly-icon3").src =
-          iconBase + data.hourly[3].weather[0].icon + ".png";
-        document.getElementById("TempHour3").innerHTML =
-          data.hourly[3].temp + "°";
+        document.getElementById("Hourly-icon3").src = iconBase + data.hourly[3].weather[0].icon + ".png";
+        document.getElementById("TempHour3").innerHTML = data.hourly[3].temp + "°";
 
         document.getElementById("TimeHour4").innerHTML = time4;
-        document.getElementById("Hourly-icon4").src =
-          iconBase + data.hourly[4].weather[0].icon + ".png";
-        document.getElementById("TempHour4").innerHTML =
-          data.hourly[4].temp + "°";
+        document.getElementById("Hourly-icon4").src = iconBase + data.hourly[4].weather[0].icon + ".png";
+        document.getElementById("TempHour4").innerHTML = data.hourly[4].temp + "°";
 
         document.getElementById("TimeHour5").innerHTML = time5;
-        document.getElementById("Hourly-icon5").src =
-          iconBase + data.hourly[5].weather[0].icon + ".png";
-        document.getElementById("TempHour5").innerHTML =
-          data.hourly[5].temp + "°";
+        document.getElementById("Hourly-icon5").src = iconBase + data.hourly[5].weather[0].icon + ".png";
+        document.getElementById("TempHour5").innerHTML = data.hourly[5].temp + "°";
         //==============================================================================================================
 
         // ========================== Väder 5 dagar framåt =============================================================
-        document.getElementById("icon-day1").src =
-          iconBase + data.daily[1].weather[0].icon + ".png";
-        document.getElementById("forcast-mintemp-day1").innerHTML =
-          "Min: " + Math.round(data.daily[1].temp.min) + "°";
-        document.getElementById("forcast-maxtemp-day1").innerHTML =
-          "Max: " + Math.round(data.daily[1].temp.max) + "°";
+        document.getElementById("icon-day1").src = iconBase + data.daily[1].weather[0].icon + ".png";
+        document.getElementById("forcast-mintemp-day1").innerHTML = "Min: " + Math.round(data.daily[1].temp.min) + "°";
+        document.getElementById("forcast-maxtemp-day1").innerHTML = "Max: " + Math.round(data.daily[1].temp.max) + "°";
 
-        document.getElementById("icon-day2").src =
-          iconBase + data.daily[2].weather[0].icon + ".png";
-        document.getElementById("forcast-mintemp-day2").innerHTML =
-          "Min: " + Math.round(data.daily[2].temp.min) + "°";
-        document.getElementById("forcast-maxtemp-day2").innerHTML =
-          "Max: " + Math.round(data.daily[2].temp.max) + "°";
+        document.getElementById("icon-day2").src = iconBase + data.daily[2].weather[0].icon + ".png";
+        document.getElementById("forcast-mintemp-day2").innerHTML = "Min: " + Math.round(data.daily[2].temp.min) + "°";
+        document.getElementById("forcast-maxtemp-day2").innerHTML = "Max: " + Math.round(data.daily[2].temp.max) + "°";
 
-        document.getElementById("icon-day3").src =
-          iconBase + data.daily[3].weather[0].icon + ".png";
-        document.getElementById("forcast-mintemp-day3").innerHTML =
-          "Min: " + Math.round(data.daily[3].temp.min) + "°";
-        document.getElementById("forcast-maxtemp-day3").innerHTML =
-          "Max: " + Math.round(data.daily[3].temp.max) + "°";
+        document.getElementById("icon-day3").src = iconBase + data.daily[3].weather[0].icon + ".png";
+        document.getElementById("forcast-mintemp-day3").innerHTML = "Min: " + Math.round(data.daily[3].temp.min) + "°";
+        document.getElementById("forcast-maxtemp-day3").innerHTML = "Max: " + Math.round(data.daily[3].temp.max) + "°";
 
-        document.getElementById("icon-day4").src =
-          iconBase + data.daily[4].weather[0].icon + ".png";
-        document.getElementById("forcast-mintemp-day4").innerHTML =
-          "Min: " + Math.round(data.daily[4].temp.min) + "°";
-        document.getElementById("forcast-maxtemp-day4").innerHTML =
-          "Max: " + Math.round(data.daily[4].temp.max) + "°";
+        document.getElementById("icon-day4").src = iconBase + data.daily[4].weather[0].icon + ".png";
+        document.getElementById("forcast-mintemp-day4").innerHTML = "Min: " + Math.round(data.daily[4].temp.min) + "°";
+        document.getElementById("forcast-maxtemp-day4").innerHTML = "Max: " + Math.round(data.daily[4].temp.max) + "°";
 
-        document.getElementById("icon-day5").src =
-          iconBase + data.daily[5].weather[0].icon + ".png";
-        document.getElementById("forcast-mintemp-day5").innerHTML =
-          "Min: " + Math.round(data.daily[5].temp.min) + "°";
-        document.getElementById("forcast-maxtemp-day5").innerHTML =
-          "Max: " + Math.round(data.daily[5].temp.max) + "°";
+        document.getElementById("icon-day5").src = iconBase + data.daily[5].weather[0].icon + ".png";
+        document.getElementById("forcast-mintemp-day5").innerHTML = "Min: " + Math.round(data.daily[5].temp.min) + "°";
+        document.getElementById("forcast-maxtemp-day5").innerHTML = "Max: " + Math.round(data.daily[5].temp.max) + "°";
         //===============================================================================================================
       });
   },
   // Sök funktionen
-  Search: function () {
+  Search: function () 
+  {
     this.FetchWeather(document.getElementById("search_box").value); // Hämtar värder ur <Input - text> med class="search-bar"
   },
 };
 
 // =========================================================== Sök funktioner ======================================================
 //Kör search funktionen om man trycker på "Sök" knappen
-document.getElementById("search_button").addEventListener("click", function () {
+document.getElementById("search_button").addEventListener("click", function () 
+{
   weatherData.Search();
   Clear();
 });
 
 //Se till att Search funktionen körs om man trycker Enter
-document
-  .getElementById("search_box")
-  .addEventListener("keyup", function (event) {
-    if (event.key == "Enter") {
+document.getElementById("search_box").addEventListener("keyup", function (event) 
+{
+    if (event.key == "Enter")
+    {
       weatherData.Search();
       Clear();
     }
   });
 
-function Clear() {
+function Clear() 
+{
   //Rensar all input i sökfältet efter varje sökning!
   document.getElementById("search_box").value = "";
 }
 // ==================================================================================================================================
 
 //========================================= Sparar Favvo Platsen ====================================================================
-function SaveFavLocation() {
+function SaveFavLocation() 
+{
   favPosiition = document.getElementById("locationName").innerHTML;
   localStorage.setItem("FavLocation", favPosiition);
 }
